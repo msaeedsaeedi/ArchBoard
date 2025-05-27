11f24583c2a5233d8dbfe1d6f13bf1f0f92d52bb
Mohammad Saeed Saeedi <msaeedsaeedi@outlook.com>
2025-05-28 00:51:33 +0500

    chore: auth refined

--- apps/frontend/src/app/guards/auth.guard.ts
+++ apps/frontend/src/app/guards/auth.guard.ts
@@ -1,9 +1,12 @@
 import { inject } from '@angular/core';
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
 export const AuthGuard: CanActivateFn = (route, state) => {
+  const router = inject(Router);
   const authService = inject(AuthService);
+  
   if (authService.isLoggedIn()) return true;
+  router.navigate(['login']);
   return false;
 };
