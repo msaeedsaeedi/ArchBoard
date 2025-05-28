6b1b7007002badabde21c2d7e11f22a46015f56d
Mohammad Saeed Saeedi <msaeedsaeedi@outlook.com>
2025-05-28 12:09:19 +0500

    feat: toasting service added

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { ToastModule } from "primeng/toast"
+import { ToastService } from './services/toast.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterModule],
+  imports: [RouterModule, ToastModule],
+  providers: [MessageService, ToastService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
