14386d2b9d612322a22c483c16555e689995ac75
Mohammad Saeed Saeedi <msaeedsaeedi@outlook.com>
2025-06-01 12:41:42 +0500

    feat(package): Shared Package added
    - Shared Password Validation rules added

---
+++ packages/shared/src/validation/password-rules.ts
@@ -0,0 +1,16 @@
+import type { StrongPasswordOptions } from 'validator';
+
+export const PasswordValidationRules: StrongPasswordOptions & { returnScore?: false } = {
+  minLength: 8,
+  minLowercase: 1,
+  minUppercase: 1,
+  minNumbers: 1,
+  minSymbols: 1,
+  returnScore: false,
+  pointsPerUnique: 1,
+  pointsPerRepeat: 0.5,
+  pointsForContainingLower: 10,
+  pointsForContainingUpper: 10,
+  pointsForContainingNumber: 10,
+  pointsForContainingSymbol: 10,
+};
