816057029e9ea5c6bf044871ff9b2b4644c52b90
Mohammad Saeed Saeedi <msaeedsaeedi@outlook.com>
2025-06-01 19:08:29 +0500

    feat(frontend): CreateBoard Feature Added

---
+++ apps/frontend/src/app/services/board.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient, HttpErrorResponse, HttpStatusCode } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { catchError, map, Observable, throwError } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BoardService {
+  http = inject(HttpClient);
+
+  create(title: string, description: string | null): Observable<string> {
+    return this.http
+      .post<{ slug: string }>(
+        `${environment.apiUrl}/boards`,
+        {
+          title,
+          description,
+        },
+        { withCredentials: true },
+      )
+      .pipe(
+        catchError((error: HttpErrorResponse) => {
+          if (error.status == HttpStatusCode.Conflict)
+            return throwError(() => Error('Board Already Exists'));
+          if (error.status == HttpStatusCode.BadRequest)
+            return throwError(() => new Error('Internal Error Occured'));
+          return throwError(() => new Error('Board Creation Failed. Please try again later'));
+        }),
+        map((value) => value.slug),
+      );
+  }
+}
