3d3e78faa4c94acd4133b551223b6f6df90c17ac
Mohammad Saeed Saeedi <msaeedsaeedi@outlook.com>
2025-06-01 12:42:20 +0500

    feat(backend): Shared Password validation rules implemented

--- apps/backend/src/auth/dto/signup.dto.ts
+++ apps/backend/src/auth/dto/signup.dto.ts
@@ -1,4 +1,11 @@
-import { IsEmail, IsOptional, IsString, IsStrongPassword, IsUrl } from 'class-validator';
+import {
+  IsEmail,
+  IsOptional,
+  IsString,
+  IsStrongPassword,
+  IsUrl,
+} from 'class-validator';
+import { PasswordValidationRules } from '@repo/shared';
 
 export class SignupDto {
   @IsString()
@@ -7,7 +14,7 @@ export class SignupDto {
   @IsEmail()
   email: string;
 
-  @IsStrongPassword()
+  @IsStrongPassword(PasswordValidationRules)
   password: string;
 
   @IsUrl()
