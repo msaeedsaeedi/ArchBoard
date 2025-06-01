dc7f0f66bd64f4bb22d3f451108088dbdd2e1d80
Mohammad Saeed Saeedi <msaeedsaeedi@outlook.com>
2025-06-01 15:15:31 +0500

    fix(frontend): commented score based password validation

--- apps/frontend/src/app/utils/password.validator.ts
+++ apps/frontend/src/app/utils/password.validator.ts
@@ -1,30 +1,32 @@
 import { AbstractControl, ValidatorFn } from '@angular/forms';
 import validator from 'validator';
-import {
-  PasswordValidationRules,
-  checkPasswordCriteria,
-  PasswordCriteriaStatus,
-} from '@repo/shared';
+import { PasswordValidationRules } from '@repo/shared';
 
 export function passwordValidator(): ValidatorFn {
   return (control: AbstractControl): { [key: string]: any } | null => {
     const value = control.value;
     if (!value) return null;
 
-    const score = validator.isStrongPassword(value, {
-      ...PasswordValidationRules,
-      returnScore: true,
-    });
-
-    const criteriaStatus: PasswordCriteriaStatus = checkPasswordCriteria(value);
-
-    // Hard-coded strong min (calculated from @repo/shared/password-rules)
-    if (score < 80) {
-      return {
-        score,
-        criteriaStatus,
-      };
-    }
-    return null;
+    const isValid = validator.isStrongPassword(value, PasswordValidationRules);
+    return isValid ? null : { weakPassword: true };
   };
 }
+
+/*
+  REFERENCE CODE (FOR IMPLEMENTING SCORE BASED PASSWORD VALIDATION)
+*/
+// const score = validator.isStrongPassword(value, {
+//   ...PasswordValidationRules,
+//   returnScore: true,
+// });
+
+// const criteriaStatus: PasswordCriteriaStatus = checkPasswordCriteria(value);
+
+// // Hard-coded strong min (calculated from @repo/shared/password-rules)
+// if (score < 80) {
+//   return {
+//     score,
+//     criteriaStatus,
+//   };
+// }
+// return null;
