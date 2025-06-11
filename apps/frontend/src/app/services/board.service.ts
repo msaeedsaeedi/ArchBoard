368d1bd2a6aae82660f3d24cb68ab53c7ac213b2
Mohammad Saeed Saeedi <msaeedsaeedi@outlook.com>
2025-06-11 18:43:18 +0500

    feat(frontend): Updated BoardService
    1. Add Collaborator REST HTTP Call

--- apps/frontend/src/app/services/board.service.ts
+++ apps/frontend/src/app/services/board.service.ts
@@ -59,4 +59,27 @@ export class BoardService {
         map((value) => value.slug),
       );
   }
+
+  addCollaborator(boardId: number, collaboratorEmail: string): Observable<boolean> {
+    return this.http
+      .post(
+        `${environment.apiUrl}/board/${boardId}/collaborators`,
+        {
+          collaboratorEmail,
+        },
+        { withCredentials: true },
+      )
+      .pipe(
+        map(() => true),
+        catchError((error: HttpErrorResponse) => {
+          if (error.status == HttpStatusCode.Conflict)
+            return throwError(() => new Error('Collaborator Already Exist'));
+          if (error.status == HttpStatusCode.NotFound)
+            return throwError(() => new Error('Collaborator Not Found'));
+          if (error.status == HttpStatusCode.Unauthorized)
+            return throwError(() => new Error('Board Not found'));
+          return throwError(() => new Error('Failed to add collaborator. Please try again later'));
+        }),
+      );
+  }
 }
