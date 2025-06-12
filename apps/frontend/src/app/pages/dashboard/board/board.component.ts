e74cd82dfdc245b72fece0629e5f6464f04ab229
Mohammad Saeed Saeedi <msaeedsaeedi@outlook.com>
2025-06-12 18:19:27 +0500

    lint fixed

--- apps/frontend/src/app/pages/dashboard/board/board.component.ts
+++ apps/frontend/src/app/pages/dashboard/board/board.component.ts
@@ -1,11 +1,9 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-board',
   imports: [],
   templateUrl: './board.component.html',
-  styleUrl: './board.component.css'
+  styleUrl: './board.component.css',
 })
-export class BoardComponent {
-  tmp = signal<string>('Hello World');
-}
+export class BoardComponent {}
