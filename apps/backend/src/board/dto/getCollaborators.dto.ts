ac20e26456fc6babedcb1d041dd5a111e8b623fc
Mohammad Saeed Saeedi <msaeedsaeedi@outlook.com>
2025-06-12 08:48:34 +0500

    fix: `getCollaboratorDto` type fixed

--- apps/backend/src/board/dto/getCollaborators.dto.ts
+++ apps/backend/src/board/dto/getCollaborators.dto.ts
@@ -1,8 +1,8 @@
-import { IsArray, IsEnum } from 'class-validator';
+import { IsEmail, IsEnum } from 'class-validator';
 import { CollaboratorRole } from 'generated/prisma';
 
 export class GetCollaboratorsDtoResponse {
-  @IsArray()
+  @IsEmail()
   email: string;
 
   @IsEnum(CollaboratorRole)
