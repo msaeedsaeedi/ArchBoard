3a062fee096f3f1d97e3ba6e04295623103e8122
msaeedsaeedi <msaeedsaeedi@outlook.com>
2025-10-11 14:38:37 +0500

    feat: NextJs project added
    - Added NextJS Project in monorepo
    - Installed ShadCN

---
+++ apps/nextjs/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+	return (
+		<div className="min-h-screen flex items-center justify-center">
+			<Button variant="outline">Button</Button>
+		</div>
+	);
+}
