ea01f73bda4cfa6f949d5820465d8981f4e88c2b
msaeedsaeedi <msaeedsaeedi@outlook.com>
2025-10-17 23:46:50 +0500

    feat(backend): initialized database

---
+++ apps/backend/db/schema.ts
@@ -0,0 +1,37 @@
+import * as p from "drizzle-orm/pg-core";
+import { pgEnum, primaryKey } from "drizzle-orm/pg-core";
+
+export const rolesEnum = pgEnum("roles", ["viewer", "editor"]);
+
+export const users = p.pgTable("users", {
+  id: p.text().primaryKey(),
+  name: p.text().notNull(),
+  image_url: p.text(),
+});
+
+export const boards = p.pgTable("boards", {
+  id: p.uuid().primaryKey(),
+  name: p.text().notNull(),
+  slug: p.text().notNull().unique(),
+  description: p.text(),
+  owner: p
+    .text()
+    .notNull()
+    .references(() => users.id),
+});
+
+export const boardCollaborators = p.pgTable(
+  "board_collaborators",
+  {
+    board_id: p
+      .uuid()
+      .notNull()
+      .references(() => boards.id),
+    user_id: p
+      .text()
+      .notNull()
+      .references(() => users.id),
+    role: rolesEnum().default("viewer"),
+  },
+  (table) => [primaryKey({ columns: [table.board_id, table.user_id] })],
+);
